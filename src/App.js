a92239e7f48302e8ccaa7dd534695ebd288f551a
JosephLapert <Josephlapert@gmail.com>
2022-04-08 22:07:19 +0200

    création du composant header

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
+import Header from './components/Header';
 import './App.css';
+
 
 function App() {
   return (
     <div className="App">
-      <h1>Hello from React</h1>
+      <Header />
     </div>
   );
 }
---
+++ src/components/Header/index.js
@@ -0,0 +1,20 @@
+
+
+
+const Header = () => {
+    return (
+        <div>
+            <nav>
+                <ul>
+                    <li><a href="#accueil">Accueil</a></li>
+                    <li><a href="#about">About</a></li>
+                    <li><a href="#project">Projects</a></li>
+                    <li><a href="#veilles">Veilles</a></li>
+                    <li><a href="#contact">Contact</a></li>
+                </ul>
+            </nav>
+        </div>
+    )
+}
+
+export default Header;
